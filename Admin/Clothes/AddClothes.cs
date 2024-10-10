94530f35aa165539a08d3c08a9e2803d4a9cd280
MinhQuan124 <dminhquan2003@gmail.com>
2024-10-10 20:31:10 +0700

    test

---
+++ Admin/Clothes/AddClothes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClothesShopManagement.Admin.Clothes
+{
+    public partial class AddClothes : Form
+    {
+        public AddClothes()
+        {
+            InitializeComponent();
+        }
+    }
+}
