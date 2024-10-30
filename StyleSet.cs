dbe2a372ca48241ada7177854580cdb8e6970d6b
DinhPhongdra <phongdra@gmail.com>
2024-10-31 01:33:41 +0700

    Add class StyleSet

---
+++ StyleSet.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClothesShopManagement
+{
+    static class StyleSet
+    {
+        public static void DataGridViewStyle (DataGridView dataGridView1 ) 
+        {
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                row.Height = 30; 
+            }
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dataGridView1.BackgroundColor = Color.White;
+            dataGridView1.AlternatingRowsDefaultCellStyle.BackColor = Color.LightGray;
+            dataGridView1.ColumnHeadersDefaultCellStyle.BackColor = Color.Navy;
+            dataGridView1.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.ColumnHeadersDefaultCellStyle.Font = new Font("Microsoft YaHei UI", 11, FontStyle.Bold);
+            dataGridView1.DefaultCellStyle.Font = new Font("Microsoft YaHei UI", 11);
+        }
+
+
+    }
+}
